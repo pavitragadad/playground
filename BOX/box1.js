8a127f725fd3e9a11912fd80f037cc5b1217415c
Unknown <pavitragadad678@gmail.com>
2018-12-05 10:58:13 +0530

    Assignment 6

---
+++ BOX/box1.js
@@ -0,0 +1,12 @@
+var width=260,height=75;
+var x1=30,y1=60;
+var x3=function()
+{
+    return x1+width-1;
+}
+console.log("x3=",+x3());
+var y3=function()
+{
+    return y1+width-1;
+}
+console.log("y3=",+y3());
---
+++ BOX/box2.js
@@ -0,0 +1,13 @@
+var page_layout_height=760;
+var page_layout_width=1024;
+
+var get_width=function()
+{
+    return (page_layout_width*10)/100;
+}
+console.log("width=",+get_width());
+var get_height=function()
+{
+    return(page_layout_height*10)/100;
+}
+console.log("height=",+get_height());
